602f4316068fe3c8f6ec96f745ecd4cda087f6d7
Starinin Andrey <starininandrey@gmail.com>
2022-07-12 18:43:07 +0300

    Add IMage.cs, IWarrior.cs

---
+++ Heroes_PD111.Lib/IMage.cs
@@ -0,0 +1,6 @@
+﻿namespace Heroes_PD111.Lib;
+
+public interface IMage
+{
+    public void MageAttack(Hero enemy);
+}
---
+++ Heroes_PD111.Lib/IWarrior.cs
@@ -0,0 +1,6 @@
+﻿namespace Heroes_PD111.Lib;
+
+public interface IWarrior
+{
+    public void WarriorAttack(Hero enemy);
+}
