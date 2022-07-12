6b4d274980bf141c95ad2b9274c135a01635e30f
Starinin Andrey <starininandrey@gmail.com>
2022-07-12 18:19:18 +0300

    Add Hero.cs

---
+++ Heroes_PD111.Lib/Hero.cs
@@ -0,0 +1,13 @@
+﻿namespace Heroes_PD111.Lib;
+
+public abstract class Hero
+{
+    protected int Health;
+    protected int Damage;
+    protected string Name;
+
+    protected Hero(string name)
+    {
+        Name = name;
+    }
+}
