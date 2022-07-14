8f0a5bbead1b31a3764f21eb3b1fab2bd2735b4c
Starinin Andrey <starininandrey@gmail.com>
2022-07-14 17:21:40 +0300

    Update test lib

--- Heroes_PD111.Lib.Test/HumanWarriorTest.cs
+++ Heroes_PD111.Lib.Test/HumanWarriorTest.cs
@@ -1,39 +1,39 @@
-using Heroes_PD111.Lib.Human;
+﻿using Heroes_PD111.Lib.Human;
 using Xunit;
 
 namespace Heroes_PD111.Lib.Test;
 
-public class HumanMageTest
+public class HumanWarriorTest
 {
     [Fact]
-    public void Create_HumanMage_Health_Test()
+    public void Create_HumanWarrior_Health_Test()
     {
         var expectedHealth = 100;
         
-        var hero = new HumanMage("");
+        var hero = new HumanWarrior("");
         var actualHealth = hero.Health;
         
         Assert.Equal(expectedHealth, actualHealth);
     }
     
     [Fact]
-    public void Create_HumanMage_Damage_Test()
+    public void Create_HumanWarrior_Damage_Test()
     {
         var expectedDamage = 100;
         
-        var hero = new HumanMage("");
+        var hero = new HumanWarrior("");
         var actualDamage = hero.Damage;
         
         Assert.Equal(expectedDamage, actualDamage);
     }
     
     [Fact]
-    public void HumanMage_MageAttack_Test()
+    public void HumanMage_WarriorAttack_Test()
     {
-        var enemy = new HumanMage("");
-        var hero = new HumanMage("");
+        var enemy = new HumanWarrior("");
+        var hero = new HumanWarrior("");
         
-        hero.MageAttack(enemy);
+        hero.WarriorAttack(enemy);
 
         var expected = 0;
         var actual = enemy.Health;
